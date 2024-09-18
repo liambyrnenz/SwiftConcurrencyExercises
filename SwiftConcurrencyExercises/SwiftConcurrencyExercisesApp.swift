40563349d07613e2fdc5e0f474906c384b261dc9
Liam Byrne <liam.jb1@gmail.com>
2024-09-18 19:53:52 +0100

    Initial Commit

---
+++ SwiftConcurrencyExercises/SwiftConcurrencyExercisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyExercisesApp.swift
+//  SwiftConcurrencyExercises
+//
+//  Created by Liam on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyExercisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
